2019a08639bc075fe8e4b22617cc4beb6cdb3507
eric82714 <0958820714ab@gmail.com>
2020-01-28 15:36:48 +0800

    Create factorial_trailing_zeroes.cpp

---
+++ C++/factorial_trailing_zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        long long ans = 0, a = 5;
+        
+        while(a <= n) {
+	        ans += (n / a);
+	        a *= 5;
+        }
+        return ans;
+    }
+};
